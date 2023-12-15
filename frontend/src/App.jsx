32a30a72d3660a101610da9a19102650d785a9d2
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-15 14:51:41 -0300

    Add dependencies and styles for menu UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,17 @@
 import {
-    BrowserRouter,
-    HashRouter,
-    Route,
-    Routes
+    HashRouter
 } from 'react-router-dom'
+import { AppRouter } from './AppRouter'
 import { Footer } from './components/UI/Footer'
 import { Header } from './components/UI/Header'
-import { AboutPage } from './components/pages/AboutPage'
-import { CreatePhonePage } from './components/pages/CreatePhonePage'
-import { EditPhonePage } from './components/pages/EditPhonePage'
-import { HomePage } from './components/pages/HomePage'
-import { MyPhonesPage } from './components/pages/MyPhonesPage'
-import { PhoneFromPage } from './components/pages/PhoneFromPage'
-import { PhoneListPage } from './components/pages/PhoneListPage'
-import { PhoneTablePage } from './components/pages/PhoneTablePage'
-import { PhonesPage } from './components/pages/PhonesPage'
-import { TransferPhonePage } from './components/pages/TransferPhonePage'
-
-function AppRouter() {
-    return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="about-page" element={<AboutPage />} />
-            <Route path="my-phones-page" element={<MyPhonesPage />} />
-            <Route path="create-phone-page" element={<CreatePhonePage />} />
-            <Route path="phone-list-page" element={<PhoneListPage />} />
-            <Route path="phones-page" element={<PhonesPage />} />
-            <Route path="phone-table-page" element={<PhoneTablePage />} />
-            <Route path="edit-phone-page" element={<EditPhonePage />} />
-            <Route path="transfer-phone-page" element={<TransferPhonePage />} />
-            <Route path="phone-from-page" element={<PhoneFromPage />} />
-        </Routes>
-    );
-}
+import { MenuUI } from './components/UI/MenuUI'
 
 function App() {
+
     return (
         <>
             <HashRouter basename="/">
+                <MenuUI />
                 <main id="main" className='min-h-screen flex flex-col justify-between bg-gray-900'>
                     <Header />
                     <AppRouter />
