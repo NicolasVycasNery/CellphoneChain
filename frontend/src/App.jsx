a3213fcf5b9787b427d94d3c35367a20d5c0ea2c
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-15 13:10:38 -0300

    Refactor routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,49 @@
-import { useState } from 'react'
+import {
+    BrowserRouter,
+    HashRouter,
+    Route,
+    Routes
+} from 'react-router-dom'
+import { Footer } from './components/UI/Footer'
+import { Header } from './components/UI/Header'
+import { AboutPage } from './components/pages/AboutPage'
+import { CreatePhonePage } from './components/pages/CreatePhonePage'
+import { EditPhonePage } from './components/pages/EditPhonePage'
+import { HomePage } from './components/pages/HomePage'
+import { MyPhonesPage } from './components/pages/MyPhonesPage'
+import { PhoneFromPage } from './components/pages/PhoneFromPage'
+import { PhoneListPage } from './components/pages/PhoneListPage'
+import { PhoneTablePage } from './components/pages/PhoneTablePage'
+import { PhonesPage } from './components/pages/PhonesPage'
+import { TransferPhonePage } from './components/pages/TransferPhonePage'
+
+function AppRouter() {
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="about-page" element={<AboutPage />} />
+            <Route path="my-phones-page" element={<MyPhonesPage />} />
+            <Route path="create-phone-page" element={<CreatePhonePage />} />
+            <Route path="phone-list-page" element={<PhoneListPage />} />
+            <Route path="phones-page" element={<PhonesPage />} />
+            <Route path="phone-table-page" element={<PhoneTablePage />} />
+            <Route path="edit-phone-page" element={<EditPhonePage />} />
+            <Route path="transfer-phone-page" element={<TransferPhonePage />} />
+            <Route path="phone-from-page" element={<PhoneFromPage />} />
+        </Routes>
+    );
+}
+
 function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Hello Vite + React!</h1>
+            <HashRouter basename="/">
+                <main id="main" className='min-h-screen flex flex-col justify-between bg-gray-900'>
+                    <Header />
+                    <AppRouter />
+                    <Footer />
+                </main>
+            </HashRouter>
         </>
     )
 }
