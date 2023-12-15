4e16da55baa1b65e3e8223d51a6c4663c22424be
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-15 14:48:31 -0300

    Update BurgerIcon size and add toggle button

--- frontend/src/components/UI/Header.jsx
+++ frontend/src/components/UI/Header.jsx
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom';
-import { slide as Menu } from 'react-burger-menu';
-import { useState } from 'react';
+
+import { useRecoilState } from 'recoil'
+import { menuState } from '../../store/BurgerMenuContext'
 
 function BurgerIcon({
-    width = "24px",
-    height = "24px",
+    width = "36px",
+    height = "36px",
 }) {
     return (
         <div style={{
@@ -47,23 +47,27 @@ function BurgerIcon({
 }
 
 function Header() {
-    const [menuOpen, setMenuOpen] = useState(false);
+    const [isOpen, setIsOpen] = useRecoilState(menuState);
     return (
         <header className="bg-gray-800 text-white p-4 shadow-lg sticky top-0 z-10">
             <div className="flex justify-between">
-            <div className="flex justify-between flex-col">
-                <h1 className="text-2xl">
-                    Cellphone Chain
-                </h1>
-                <h2 className="text-lg">
-                    DAPP Demo
-                </h2>
+                <div className="flex justify-between flex-col">
+                    <h1 className="text-2xl">
+                        Cellphone Chain
+                    </h1>
+                    <h2 className="text-lg">
+                        DAPP Demo
+                    </h2>
+                </div>
+                <div className="flex justify-between m-2">
+                    <button onClick={() => setIsOpen(!isOpen)}>
+                    <BurgerIcon />
+                    </button>
+                </div>
             </div>
-            <div className="flex justify-between m-2">
-                <BurgerIcon />
+            <div>
+                
             </div>
-            </div>
-
         </header>
     );
 }
@@ -71,15 +75,5 @@ function Header() {
 export { Header };
 
 /**
-<Menu className='bg-gray-800 text-white' pageWrapId={"pushRotate"} disableOverlayClick isOpen={menuOpen} onStateChange={(state) => { }}>
-                <Link className="bg-gray-700 hover:bg-gray-600 p-2 border-b border-gray-800" to="/">Home</Link>
-                <Link className="bg-gray-700 hover:bg-gray-600 p-2 border-b border-gray-800" to="/about-page">About</Link>
-                <Link className="bg-gray-700 hover:bg-gray-600 p-2 border-b border-gray-800" to="/my-phones-page">My Phones</Link>
-                <Link className="bg-gray-700 hover:bg-gray-600 p-2 border-b border-gray-800" to="/create-phone-page">Create Phone</Link>
-                <Link className="bg-gray-700 hover:bg-gray-600 p-2 border-b border-gray-800" to="/phone-list-page">Phone List</Link>
-                <Link className="bg-gray-700 hover:bg-gray-600 p-2 border-b border-gray-800" to="/phones-page">Phones</Link>
-                <Link className="bg-gray-700 hover:bg-gray-600 p-2 border-b border-gray-800" to="/phone-table-page">Phone Table</Link>
-                <Link className="bg-gray-700 hover:bg-gray-600 p-2 border-b border-gray-800" to="/edit-phone-page">Edit Phone</Link>
-                <Link className="bg-gray-700 hover:bg-gray-600 p-2 border-b border-gray-800" to="/transfer-phone-page">Transfer Phone</Link>
-            </Menu>
+
  */
