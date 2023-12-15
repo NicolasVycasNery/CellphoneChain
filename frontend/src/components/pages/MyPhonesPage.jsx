8855c8fe491809a36d10266632a185053cf6d15b
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-15 15:20:22 -0300

    Refactor MyPhonesPage component to fetch and display phones owned by the user

--- frontend/src/components/pages/MyPhonesPage.jsx
+++ frontend/src/components/pages/MyPhonesPage.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+import { getPhonesFromOwner } from "../../services/web3";
+import { PhoneCard } from "../phones/PhoneCard";
+
 export function MyPhonesPage() {
-    return (<>
-    
-    </>)
+    const [phones, setPhones] = useState([]);
+    useEffect(() => {
+        async function load() {
+            const phones = await getPhonesFromOwner(
+                window.ethereum.selectedAddress
+            );
+            setPhones(phones);
+        }
+        load();
+    }, []);
+    return (
+        <section className="grid grid-cols-3 gap-4">
+            {phones.map(phone => (
+                <PhoneCard key={phone.id} phone={phone} isOwner={true} />
+            ))}
+        </section>
+    )
 }
