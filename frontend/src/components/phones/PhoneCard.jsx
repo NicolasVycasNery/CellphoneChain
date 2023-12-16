f1047356598f18df98d5d5e72b0ba446de8e8337
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-16 15:31:40 -0300

    Refactor PhoneCard component to include owner information

--- frontend/src/components/phones/PhoneCard.jsx
+++ frontend/src/components/phones/PhoneCard.jsx
@@ -4,8 +4,8 @@ import Swal from "sweetalert2";
 
 export function PhoneCard({ phone, isOwner = false }) {
 
-    const { model, brand, price, id } = phone;
-    
+    const { id, model, brand, price, owner } = phone;
+
     async function handleDelete(e) {
         e.preventDefault();
         const confirm = await Swal.fire({
@@ -42,23 +42,34 @@ export function PhoneCard({ phone, isOwner = false }) {
             })
         }
     }
-    
+
     return (
         <div className="flex flex-col gap-2 text-white bg-gray-800 p-4 rounded shadow-lg">
-            <p>ID:
+            <p
+                className="text-2xl font-bold"
+            >ID:
                 <span className="text-green-500">
                     {id}
                 </span>
             </p>
-            <p>Model: {model}</p>
+            <p
+            >Model: {model}</p>
             <p>Brand: {brand}</p>
             <p>Price: {price}</p>
+            <p
+                className="text-sm font-bold break-all"
+            >Owner:
+                <span className="text-yellow-500">
+                    {owner}
+                </span>
+            </p>
             {
                 isOwner && (
-                    <div className="flex flex-row gap-2">
+                    <div className="flex flex-col gap-2 text-lg">
                         <Link
                             to={`/edit-phone-page?id=${id}`}
-                            className="bg-green-500 p-2 rounded shadow-lg">
+                            className="bg-green-500 p-2 rounded shadow-lg text-center"
+                        >
                             Edit
                         </Link>
                         <button onClick={handleDelete} className="bg-red-500 p-2 rounded shadow-lg">Delete</button>
