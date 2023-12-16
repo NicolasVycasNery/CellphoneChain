d31615ab19f2474c278dba6a2f2944212761e4da
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-16 15:57:34 -0300

    Remove About link from HomePage

--- frontend/src/components/pages/HomePage.jsx
+++ frontend/src/components/pages/HomePage.jsx
@@ -16,9 +16,6 @@ export function HomePage() {
                 </p>
             </div>
             <div className="flex space-x-4">
-                <Link to="/about-page" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    About
-                </Link>
                 <Link to="/my-phones-page" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     List Your Phones
                 </Link>
