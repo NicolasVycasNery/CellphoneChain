dbf54bb621cd3a9f83c5d09b0bdc6852ecc1bc6f
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-16 14:19:17 -0300

    Refactor PhoneTableHeader component to accept columnNames prop

--- frontend/src/components/phones/PhoneTableHeader.jsx
+++ frontend/src/components/phones/PhoneTableHeader.jsx
@@ -1,5 +1,15 @@
-export function PhoneTableHeader() {
-    return (<>
-
-    </>)
+export function PhoneTableHeader({
+    columnNames = ["ID", "Model", "Manufacturer", "Price"],
+}) {
+    return (
+        <thead>
+            <tr className="bg-gray-700 text-white">
+                {columnNames.map((columnName) => (
+                    <th
+                        className="bg-gray-800 text-white text-left px-4 py-2"
+                        key={columnName}>{columnName}</th>
+                ))}
+            </tr>
+        </thead>
+    )
 }
