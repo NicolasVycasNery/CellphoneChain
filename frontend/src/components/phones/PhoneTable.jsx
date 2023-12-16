85588ea32c3c6bf91393f0e974aeb9b5ec2baa50
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-16 15:57:58 -0300

    Refactor PhoneTable component to display phones dynamically

--- frontend/src/components/phones/PhoneTable.jsx
+++ frontend/src/components/phones/PhoneTable.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { getPhonesPage } from "../../services/web3";
-import { PhoneTableRow } from "./PhoneTableRows";
+import { PhoneTableRow } from "./PhoneTableRow";
 import { PhoneTableHeader } from "./PhoneTableHeader";
 
 export function PhoneTable({ page, limit }) {
     const [phones, setPhones] = useState([]);
-    
+
     async function load() {
         const phones = await getPhonesPage(page, limit);
         setPhones(phones);
@@ -17,14 +17,23 @@ export function PhoneTable({ page, limit }) {
 
     useEffect(() => {
         load();
-    }, [page, pageSize]);
+    }, [page, limit]);
 
-    return (
-        <table className="table-auto">
-            <PhoneTableHeader />
-            <tbody>
-                <PhoneTableRow />
-            </tbody>
-        </table>
-    )
+    return (<>
+        {phones.length === 0 && (<h1 className="text-white text-2xl text-center col-span-3">
+            No phones found.
+        </h1>)}
+        {phones.length > 0 && (
+            <table className="table-auto">
+                <PhoneTableHeader />
+                <tbody>
+                    {phones.map((phone, index) => {
+                        return (
+                            <PhoneTableRow key={phone.id} phone={phone} index={index} />
+                        )
+                    })}
+                </tbody>
+            </table>
+        )}
+    </>)
 }
