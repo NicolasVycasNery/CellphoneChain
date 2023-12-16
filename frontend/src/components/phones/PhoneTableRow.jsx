54773a7976930b02b4874c269e4e00ab110581cb
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-16 14:19:22 -0300

    Refactor PhoneTableRow component

--- frontend/src/components/phones/PhoneTableRow.jsx
+++ frontend/src/components/phones/PhoneTableRow.jsx
@@ -1,5 +1,10 @@
 export function PhoneTableRow({ phone }) {
-    return (<>
-
-    </>)
+    return (
+        <tr className="bg-gray-700 text-white">
+            <td className="bg-gray-800 text-white text-left px-4 py-2">{phone.id}</td>
+            <td className="bg-gray-800 text-white text-left px-4 py-2">{phone.model}</td>
+            <td className="bg-gray-800 text-white text-left px-4 py-2">{phone.manufacturer}</td>
+            <td className="bg-gray-800 text-white text-left px-4 py-2">{phone.price}</td>
+        </tr>
+    )
 }
