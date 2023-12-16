51d6c5466c202404f087bda9f8c8dbc4c04d766f
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-16 14:19:12 -0300

    Refactor PhoneTable component to fetch and display phones dynamically

--- frontend/src/components/phones/PhoneTable.jsx
+++ frontend/src/components/phones/PhoneTable.jsx
@@ -1,5 +1,30 @@
-export function PhoneTable({ page, limit }) {
-    return (<>
+import { useEffect, useState } from "react";
+import { getPhonesPage } from "../../services/web3";
+import { PhoneTableRow } from "./PhoneTableRows";
+import { PhoneTableHeader } from "./PhoneTableHeader";
 
-    </>)
+export function PhoneTable({ page, limit }) {
+    const [phones, setPhones] = useState([]);
+    
+    async function load() {
+        const phones = await getPhonesPage(page, limit);
+        setPhones(phones);
+    }
+
+    useEffect(() => {
+        load();
+    }, []);
+
+    useEffect(() => {
+        load();
+    }, [page, pageSize]);
+
+    return (
+        <table className="table-auto">
+            <PhoneTableHeader />
+            <tbody>
+                <PhoneTableRow />
+            </tbody>
+        </table>
+    )
 }
