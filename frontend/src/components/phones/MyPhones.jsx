87c6994cd777d20b340e4c061d1fca02c8fed510
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-16 14:18:58 -0300

    Add pagination and page size options to MyPhones component

--- frontend/src/components/phones/MyPhones.jsx
+++ frontend/src/components/phones/MyPhones.jsx
@@ -1,5 +1,34 @@
-export function MyPhones() {
-    return (<>
+import { useState } from 'react';
+import { PhoneList } from '../phones/PhoneList';
 
-    </>)
+export function MyPhones() {
+    const [page, setPage] = useState(0);
+    const [pageSize, setPageSize] = useState(10);
+
+    return (
+        <section className="flex flex-col items-center">
+            <h1 className="text-2xl text-white my-5">Phone List</h1>
+            <div className="flex flex-row gap-2">
+                <div>
+                    <label htmlFor="page">Page</label>
+                    <input
+                        type="number"
+                        name="page"
+                        id="page"
+                        value={page}
+                        onChange={(e) => setPage(e.target.value)}
+                        className="bg-gray-800 text-white rounded shadow-lg"
+                    />
+                </div>
+                <div>
+                    <select name="pageSize" id="pageSize" value={pageSize} onChange={(e) => setPageSize(e.target.value)} className="bg-gray-800 text-white rounded shadow-lg">
+                        <option value="10">12</option>
+                        <option value="20">25</option>
+                        <option value="50">50</option>
+                    </select>
+                </div>
+            </div>
+            <PhoneList page={page} limit={pageSize} isOwner={true} />
+        </section>
+    )
 }
