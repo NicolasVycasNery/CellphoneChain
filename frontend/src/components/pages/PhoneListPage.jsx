34582dd647dc5317aab41016cf581d9edbc15968
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-16 15:57:44 -0300

    Update page and page size in PhoneListPage component

--- frontend/src/components/pages/PhoneListPage.jsx
+++ frontend/src/components/pages/PhoneListPage.jsx
@@ -3,21 +3,28 @@ import { PhoneList } from '../phones/PhoneList';
 
 
 export function PhoneListPage() {
-    const [page, setPage] = useState(0);
-    const [pageSize, setPageSize] = useState(10);
+    const [page, setPage] = useState(1);
+    const [pageSize, setPageSize] = useState(12);
 
     return (
-        <section className="flex flex-col items-center">
+        <section className="flex flex-col gap-2 align-top m-5">
             <h1 className="text-2xl text-white my-5">Phone List</h1>
             <div className="flex flex-row gap-2">
                 <div>
-                    <label htmlFor="page">Page</label>
+                    <label className='text-white' htmlFor="page">Page:</label>
                     <input
                         type="number"
                         name="page"
                         id="page"
                         value={page}
-                        onChange={(e) => setPage(e.target.value)}
+                        onChange={(e) => {
+                            // page cannot be less than 1
+                            if (e.target.value < 1) {
+                                setPage(1);
+                            } else {
+                                setPage(e.target.value);
+                            }
+                        }}
                         className="bg-gray-800 text-white rounded shadow-lg"
                     />
                 </div>
