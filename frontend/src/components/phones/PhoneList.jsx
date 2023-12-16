79faa763c6caf9bae72a402957b3fe92098abb1c
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-16 14:19:07 -0300

    Refactor PhoneList component to use hooks and add pagination support

--- frontend/src/components/phones/PhoneList.jsx
+++ frontend/src/components/phones/PhoneList.jsx
@@ -1,5 +1,30 @@
-export function PhoneList({ page, limit }) {
-    return (<>
+import { useEffect, useState } from "react";
+import { getPhonesPage } from "../../services/web3";
+import { PhoneCard } from "./PhoneCard";
 
-    </>)
+export function PhoneList({ page, limit, isOwner = false }) {
+    const [phones, setPhones] = useState([]);
+    async function load() {
+        const phones = await getPhonesPage(page, limit);
+        setPhones(phones);
+    }
+
+    useEffect(() => {
+        load();
+    }, []);
+
+    useEffect(() => {
+        load();
+    }, [page, pageSize]);
+
+    return (
+        <div className="grid grid-cols-3 gap-4">
+            {phones.length === 0 && <p className="text-white">No phones found</p>}
+            {phones.map((phone) => {
+                return (
+                    <PhoneCard key={phone.id} phone={phone} isOwner={isOwner} />
+                )
+            })}
+        </div>
+    )
 }
