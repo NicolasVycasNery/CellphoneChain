66730f36c5ac16bf9ee0f45a1e2dd439a2bb73b0
Nicolas Vycas Nery <nicolas@ditavia.com>
2023-12-16 16:13:46 -0300

    Add TransferPhoneForm to TransferPhonePage

--- frontend/src/components/pages/TransferPhonePage.jsx
+++ frontend/src/components/pages/TransferPhonePage.jsx
@@ -1,5 +1,12 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { TransferPhoneForm } from '../phones/TransferPhoneForm';
+
 export function TransferPhonePage() {
-    return (<>
-    
-    </>)
+    const { id } = useParams();
+    return (
+        <section className="flex flex-col items-center">
+            <h1 className="text-2xl text-white my-5">Transfer Phone</h1>
+            <TransferPhoneForm id={id} />
+        </section>)
 }
